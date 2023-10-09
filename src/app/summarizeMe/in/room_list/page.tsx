3ea873f5218ac1aeb55143877b76fc471eb87967
shimizu <noon3m.op.sh@gmail.com>
2023-10-09 12:05:21 +0900

    [update]popの中身を追加

--- src/app/summarizeMe/in/room_list/page.tsx
+++ src/app/summarizeMe/in/room_list/page.tsx
@@ -10,6 +10,10 @@ export default function Page() {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleModal = async () => {
+    setIsOpen(!isOpen);
+  };
+
+  const send = async () => {
     console.log("open");
     try {
       const url = "http://localhost:8080/room/add";
@@ -39,8 +43,8 @@ export default function Page() {
     } catch (error) {
       console.error("Error during fetch:", error);
     }
-    setIsOpen(!isOpen);
-  };
+    setIsOpen(isOpen);
+  }
 
   return (
     <div className="w-full">
@@ -70,7 +74,49 @@ export default function Page() {
         </div>
       </div>
 
-      {isOpen && <div onClick={toggleModal} className="bg-slate-500/75 fixed w-full h-full top-0 left-0"></div>}
+      {isOpen && (
+        <div onClick={toggleModal} className="bg-slate-500/75 fixed w-full h-full top-0 left-0 flex min-h-screen justify-center items-center">
+          <div
+            onClick={(e) => {
+              e.stopPropagation();
+            }}
+            className="rounded-lg bg-sub-color w-1/3 h-1/3 py-3 shadow-lg shadow-sub-color"
+          >
+            <div className="h-full flex flex-col justify-around items-center">
+              <div className="flex justify-center items-center w-full px-3">
+                <hr className="border-2 border-black w-2/6 mx-2" />
+                <span className="text-center text-3xl font-extrabold w-2/6">ルーム作成</span>
+                <hr className="border-2 border-black w-2/6 mx-2" />
+              </div>
+
+              <div className="flex w-full justify-center items-center">
+                <span onChange={(event) => console.log(event.target)} className="font-bold text-2xl">
+                  作成時間：
+                </span>
+                <select id="times" className="rounded-lg font-bold text-2xl bg-opacity-100 block w-1/3 h-2/3 pl-2.5 my-5 border-2 border-black">
+                  <option className="font-bold" value="15">
+                    15分
+                  </option>
+                  <option className="font-bold" value="30">
+                    30分
+                  </option>
+                  <option className="font-bold" value="60">
+                    60分
+                  </option>
+                </select>
+              </div>
+
+              <button onClick={send} className="w-2/6 rounded-lg font-bold bg-black text-sub-color text-center m-4  px-8 py-4">
+                作成
+              </button>
+
+              <div className="w-full px-4">
+                <hr className="border-2 border-black w-full" />
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
 
       <div className="w-full flex flex-wrap font-bold text-2xl">
         <div className="w-1/3 h-96 p-4">
